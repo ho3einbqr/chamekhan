6a102e2bfa44e54839f3d8b6678f1fdb6ce62b8a
Hosein Bagheri <ho3ein.bqr@gmail.com>
2020-07-22 20:20:18 +0430

    GraphicConstants class defined

---
+++ lib/common/constants/graphic_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'string_constants.dart';
+
+class GraphicConstants {
+  static const logo = Text(StringConstants.appNameFa,style: TextStyle(fontSize: 30),);
+}
