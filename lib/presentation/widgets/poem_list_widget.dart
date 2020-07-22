1c7f6362307f1c378bb5e3bbad63335e420e61cd
Hosein Bagheri <ho3ein.bqr@gmail.com>
2020-07-22 20:40:43 +0430

    PoemList class defined

---
+++ lib/presentation/widgets/poem_list_widget.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class PoemList extends StatefulWidget {
+  @override
+  _PoemListState createState() => _PoemListState();
+}
+
+class _PoemListState extends State<PoemList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a  is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a  is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a  is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a  is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a  is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+        Text(
+            'this is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis is a testthis '
+        ),
+      ],
+    );
+  }
+}
