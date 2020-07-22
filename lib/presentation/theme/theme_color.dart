66774dcaac735b86559d68d9b48fd3c092963983
Hosein Bagheri <ho3ein.bqr@gmail.com>
2020-07-22 20:27:20 +0430

    AppColor class defined some color defined

---
+++ lib/presentation/theme/theme_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xffd63d41);
+
+  static Color shadowColor = Color(0xffbcbcbc).withOpacity(0.8);
+
+  static Color eColorAlpha100 = Color(0xffEEEEEE).withAlpha(100);
+
+  static const Color buttonColor = Color(0xff757575);
+  static Color buttonColorAlpha200 = Color(0xff757575).withAlpha(200);
+  static Color buttonColorAlpha255 = Color(0xff757575).withAlpha(255);
+  static const Color buttonSplashColor = Color(0xffbcbcbc);
+
+}
