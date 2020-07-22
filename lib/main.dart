8dc56bb1135f7898db4314e0bbb68578f596befe
Hosein Bagheri <ho3ein.bqr@gmail.com>
2020-07-22 20:51:25 +0430

    main file changed

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:chamekhan/presentation/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChameKhanApp());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
