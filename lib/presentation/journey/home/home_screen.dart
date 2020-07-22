d2bb5aa4173fd6ecb6486b59f47dbe6f162b8e0b
Hosein Bagheri <ho3ein.bqr@gmail.com>
2020-07-22 20:44:44 +0430

    HomeScreen class defined

---
+++ lib/presentation/journey/home/home_screen.dart
@@ -0,0 +1,119 @@
+import 'dart:ui';
+
+import 'package:chamekhan/common/constants/graphic_constants.dart';
+import 'package:chamekhan/common/utils/bar_clipper/app_bar_clipper.dart';
+import 'package:chamekhan/common/utils/bar_clipper/bottom_bar_clipper.dart';
+import 'package:chamekhan/presentation/theme/theme_color.dart';
+import 'package:chamekhan/presentation/widgets/app_bar_button.dart';
+import 'package:chamekhan/presentation/widgets/bar.dart';
+import 'package:chamekhan/presentation/widgets/poem_list_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ScrollController _changeFABOpacity;
+  Color _fabColor;
+
+  @override
+  initState() {
+    super.initState();
+    _fabColor = AppColor.buttonColorAlpha255;
+    _changeFABOpacity = new ScrollController();
+    _changeFABOpacity.addListener(() {
+      if (_changeFABOpacity.position.userScrollDirection ==
+          ScrollDirection.reverse &&
+          _fabColor == AppColor.buttonColorAlpha255)
+        setState(() {
+          _fabColor = AppColor.buttonColorAlpha200;
+        });
+      if (_changeFABOpacity.position.userScrollDirection ==
+          ScrollDirection.forward &&
+          _fabColor == AppColor.buttonColorAlpha200)
+        setState(() {
+          _fabColor = AppColor.buttonColorAlpha255;
+        });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: Stack(
+        children: [
+          ListView(
+            controller: _changeFABOpacity,
+            scrollDirection: Axis.vertical,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(35),
+              ),
+              PoemList(),
+              Padding(
+                padding: EdgeInsets.all(45),
+              ),
+            ],
+          ),
+          //AppBar
+          Positioned(
+            top: 0,
+            child: Bar(80,Offset(0,3),AppBarClipper()),
+          ),
+          //Logo
+          Positioned(
+            top: 40,
+            left: 0,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GraphicConstants.logo
+              ],
+            ),
+          ),
+          //AppBar Buttons
+          Positioned(
+            top: 30,
+            width: MediaQuery.of(context).size.width,
+            child: Wrap(
+              alignment: WrapAlignment.spaceBetween,
+              children: <Widget>[
+                AppBarButton(Icons.info),
+                AppBarButton(Icons.account_circle),
+              ],
+            ),
+          ),
+          //FAB
+          Positioned(
+            bottom: 30,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: _fabColor,
+                  splashColor: AppColor.buttonSplashColor,
+                  child: Icon(
+                    Icons.add,
+                    color: AppColor.eColorAlpha100,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //BottomBar
+          Positioned(
+            bottom:0,
+            child: Bar(50,Offset(0,-3),BottomBarClipper()),
+          ),
+        ],
+      ),
+    );
+  }
+}
