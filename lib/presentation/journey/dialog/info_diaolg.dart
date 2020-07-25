769753deac3e6cc7bd590bc0ef5b371c4454a901
Hosein Bagheri <ho3ein.bqr@gmail.com>
2020-07-25 13:58:55 +0430

    InfoDialog class defined

---
+++ lib/presentation/journey/dialog/info_diaolg.dart
@@ -0,0 +1,29 @@
+import 'package:chamekhan/presentation/widgets/custom_dialog.dart';
+import 'package:flutter/material.dart';
+
+class InfoDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomDialog(
+      icon: Icon(
+        Icons.info,
+        size: 30,
+      ),
+      children: <Widget>[
+        Text(
+          'name family / nickname',
+          style: TextStyle(fontSize: 20),
+        ),
+        // SizedBox(height: 20,),
+        Text('Phone number: 09103593673'),
+        FlatButton(
+          child: Text('Log out'),
+          color: Colors.red,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+}
