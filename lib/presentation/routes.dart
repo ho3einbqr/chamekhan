6cf4fb8717f2339677712c455684554860379feb
Hosein Bagheri <ho3ein.bqr@gmail.com>
2020-08-01 15:40:39 +0430

    new case added to route switch also name added to routes setting

--- lib/presentation/routes.dart
+++ lib/presentation/routes.dart
@@ -1,4 +1,5 @@
 import 'package:chamekhan/common/constants/router_constants.dart';
+import 'package:chamekhan/presentation/journey/Input/first_input_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -6,12 +7,17 @@ import 'journey/home/home_screen.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name){
+    switch (settings.name) {
       case RouteName.initial:
         return null;
       case RouteName.homeScreen:
-        return MaterialPageRoute(builder: (_)=> HomeScreen());
+        return MaterialPageRoute(
+            settings: RouteSettings(name: RouteName.homeScreen),
+            builder: (_) => HomeScreen());
+      case RouteName.firstInputScreen:
+        return MaterialPageRoute(
+            settings: RouteSettings(name: RouteName.firstInputScreen),
+            builder: (_) => Page1());
     }
   }
-
 }
