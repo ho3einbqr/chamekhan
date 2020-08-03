31e549fb52196c7a2e600f6f5c70a9777bf66776
Hosein Bagheri <ho3ein.bqr@gmail.com>
2020-08-03 14:42:19 +0430

    CardEntity class updated needs attr. define

--- lib/domain/entities/card_entity.dart
+++ lib/domain/entities/card_entity.dart
@@ -1,6 +1,6 @@
-class ListTest {
+class CardEntity {
   final String name;
   final String poet;
   final String type;
-  ListTest(this.name, this.poet, this.type);
+  CardEntity(this.name, this.poet, this.type);
 }
